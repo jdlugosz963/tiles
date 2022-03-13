dcac24e12f88b608fdc9e8dd232949e4f72e1ee4
jdlugosz963 <jdlugosz963@gmail.com>
2022-03-13 22:32:26 +0100

    Remove color model and replace with const colors.

--- hipis/tiles/models.py
+++ hipis/tiles/models.py
@@ -1,15 +1,20 @@
 from django.db import models
 
-class Color(models.Model):
-    name = models.CharField(max_length=15)
-    hexcolor = models.CharField(max_length=7)
-
-    def __str__(self):
-        return self.name
-
 class Category(models.Model):
     name = models.CharField(max_length=15)
-    color = models.ForeignKey(Color, on_delete=models.CASCADE)
+    color = models.CharField(
+        max_length=15,
+        choices = (
+            ('bg-red-600', 'Red'),
+            ('bg-green-600', 'Blue'),
+            ('bg-lime-600', 'Lime'),
+            ('bg-emerald-600', 'Emerald'),
+            ('bg-indigo-600', 'Indigo'),
+            ('bg-violet-600', 'Violet'),
+            ('bg-rose-600', 'Rose')
+        ),
+        default = 'Lime'
+    )
 
     def __str__(self):
         return self.name
