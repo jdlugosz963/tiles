da2caec97d8fa6530dcc3442b81adbb1d34197cf
jdlugosz963 <jdlugosz963@gmail.com>
2022-03-13 20:37:35 +0100

    add placeholders for forms.

--- hipis/tiles/forms.py
+++ hipis/tiles/forms.py
@@ -2,12 +2,22 @@ from django import forms
 from .models import Tile, Category
 
 class TileForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(TileForm, self).__init__(*args, **kwargs)
+        for key, value in self.fields.items():
+            value.widget.attrs['placeholder'] = key.capitalize()
+
     class Meta:
         model=Tile
         fields = ['name', 'description', 'category']
 
 
 class CategoryForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(CategoryForm, self).__init__(*args, **kwargs)
+        for key, value in self.fields.items():
+            value.widget.attrs['placeholder'] = key.capitalize()
+
     class Meta:
         model=Category
         fields = '__all__'
